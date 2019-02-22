3323cdf1f17e8bf8b255f5d855756d996c169424
ejohns11-2019 <47802878+ejohns11-2019@users.noreply.github.com>
2019-02-21 23:04:45 -0700

    Add files via upload
    Add relative path to person class

---
+++ wallet.rb
@@ -0,0 +1,14 @@
+class Wallet
+require_relative 'Person'
+  attr_accessor :user
+
+  def initialize(user)
+    user = @user
+  end
+
+  def show_wallet(user)
+      puts #?????????
+   user[:name, :money]
+
+  end
+end
